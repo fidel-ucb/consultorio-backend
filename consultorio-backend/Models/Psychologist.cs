be838f29081e3ffc6259356b53a124d8b4bd7631
Fidel Montecinos <fidel.montecinos@ucb.edu.bo>
2026-08-31 18:31:00 -0400

    feat: estandarizar el uso de tipos de referencia nulables en los modelos de dominio

--- consultorio-backend/Models/Psychologist.cs
+++ consultorio-backend/Models/Psychologist.cs
@@ -1,8 +1,13 @@
-﻿namespace consultorio_backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace consultorio_backend.Models
 {
     public class Psychologist : Profile
     {
+        [Required]
         public string LicenceNumber { get; set; } = string.Empty;
+
+        [Required]
         public string Specialty { get; set; } = string.Empty;
     }
 }
